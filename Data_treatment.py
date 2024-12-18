1cf2d7ff01b519a24bbb50c585b0d3cd33d06f22
agnesp24 <agnes.beatriz01@gmail.com>
2024-12-18 14:49:09 -0300

    Final complete

--- Data_treatment.py
+++ Data_treatment.py
@@ -24,6 +24,9 @@ def limpeza():
     #Removing column "PAÍS"
     dataf.drop('pais', axis=1, inplace=True)
 
+    #Changing date format
+    dataf['data_correct'] = pandas.to_datetime(dataf['data'], format='%d/%m/%Y', errors='coerce')
+
     #Renaming columns
     dataf = dataf.rename(columns={'nome': 'NOME', 'cpf': 'CPF', 'idade': 'IDADE', 'data': 'DATA NASC'})
 
@@ -72,25 +75,43 @@ def final():
     pandas.set_option('display.width', None)
     pandas.set_option('display.max_colwidth', None)
 
-    #Máscara para dados sensíveis
+    #Mask for sensitive data
     dataf['CPF'] = dataf['CPF'].apply(lambda x: f'{x[:3]}.***.***-{x[-2:]}')
 
-    #Separação do endereço em diferentes campos
+    #Correcting the date of birth to the correct format
+    dataf['DATA NASC'] = pandas.to_datetime(dataf['DATA NASC'], format='%d/%m/%Y', errors="coerce")
+    #Defining a variable for the current date
+    current_date = pandas.to_datetime('today')
+    #Updating the age based on the current date.
+    dataf['IDADE'] = current_date.year - dataf['DATA NASC'].dt.year
+    #Coverting age to int.
+    dataf['IDADE'] = dataf['IDADE'].astype('Int64')
+    #Calculating the correct age based on month and day of the current year.
+    dataf['IDADE'] -= ((current_date.month <= dataf['DATA NASC'].dt.month) & (current_date.day < dataf['DATA NASC'].dt.day)).astype(int)
+    #Where we have wrong dates, we are calculating the year of birth based on the age.
+    dataf['DATA NASC'] = dataf['DATA NASC'].where(dataf['DATA NASC'] < current_date, pandas.to_datetime(current_date.year - dataf['IDADE'], format='%Y', errors='coerce'))
+    dataf.loc[dataf['IDADE'] > 100, 'IDADE'] = numpy.nan
+
+    #Changing the address field to separate fields.
     dataf['ENDEREÇO'] = dataf['endereco'].apply(lambda x: x.split('\n')[0].strip())
     dataf['BAIRRO'] = dataf['endereco'].apply(lambda x: x.split('\n')[1].strip() if len(x.split('\n')) > 1 else x)
     dataf['ESTADO'] = dataf['endereco'].apply(lambda x: x.split(' / ')[-1].strip() if len(x.split('\n')) > 1 else x)
 
-    #Correção de dados errôneos
+    #Corrrecting wrong data
     dataf['CPF'] = dataf['CPF'].apply(lambda x: 'INVÁLIDO' if len(x) < 14 else x)
 
     estadosbr = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB',
     'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
     dataf['ESTADO'] = dataf['ESTADO'].apply(lambda x: x if x in estadosbr else 'INVÁLIDO')
 
+    dataf['NOME'] = dataf['NOME'].apply(lambda x: 'NOME INVÁLIDO' if isinstance(x,str) and len(x) > 50 else x)
+
     dataf_new = dataf[['NOME', 'CPF', 'IDADE', 'DATA NASC', 'ENDEREÇO', 'BAIRRO', 'ESTADO']]
-    print(dataf_new.head(30))
+    print(dataf_new.head(50))
+
+    dataf_new.to_csv('clientes_final.csv')
 
 #tratamento()
 #limpeza()
 #outliers()
-final()
+#final()
